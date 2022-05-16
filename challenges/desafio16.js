3f9b87fa89b2be794c1315c3d2a2bbd8fd919cc5
lucas <lucmaieski@gmail.com>
2022-05-16 19:15:54 -0300

    Feito requistio 16

---
+++ challenges/desafio16.js
@@ -0,0 +1,21 @@
+db.produtos.updateMany(
+    {
+        nome: 'Big Mac'
+    },
+    {
+        $currentDate: {
+            ultimaModificacao: {$type: 'timestamp'}
+        }
+    }
+)
+
+db.produtos.find(
+    {
+        ultimaModificacao: { $exists: true },
+    }
+    , 
+    {
+        nome: true,
+        _id: false
+    }
+)
