5d10b3cf0a29fd2adf75daea665a9d65e2d5cf7b
lucas <lucmaieski@gmail.com>
2022-05-16 18:45:03 -0300

    Feito requistio 15

---
+++ challenges/desafio15.js
@@ -0,0 +1,29 @@
+// Adicione o campo avaliacao em todos os documentos da coleção e efetue alterações nesse campo
+
+db.produtos.updateMany({}, {
+    $set: {avaliacao: NumberInt(0)}
+})
+
+db.produtos.updateMany(
+    {
+      tags: { $in: ["bovino"] },
+    },
+    {
+      $inc: { avaliacao: 5 },
+    },
+  );
+  
+  db.produtos.updateMany(
+    {
+      tags: { $in: ["ave"] },
+    },
+    {
+      $inc: { avaliacao: 3 },
+    },
+  );
+
+db.produtos.find({}, {
+    nome: true,
+    avaliacao: true,
+    _id: false
+})
