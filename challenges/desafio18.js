86e315ac733a3cd9343506f1890fda9d018b5a78
lucas <lucmaieski@gmail.com>
2022-05-16 19:25:30 -0300

    Feito requistio 18

---
+++ challenges/desafio18.js
@@ -0,0 +1,17 @@
+db.produtos.updateMany({
+    $or: [
+        {nome: 'Big Mac'},
+        {nome: 'Quarteirão com Queijo'}
+    ]
+}, {
+    $addToSet: {
+        ingredientes: 'bacon'
+    }
+})
+
+db.produtos.find({}, 
+    {
+        nome: true,
+        ingredientes: true,
+        _id: false
+    })
